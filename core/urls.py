cf722d14db656b6b62f15f08c8a3c8d6e8842528
Anton Elesin <elr1c.edw180@gmail.com>
2024-08-12 17:16:51 +0300

    new_user

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 urlpatterns = [
     path('create_user/', views.create_user),
-    path('user/<int:pk>/', views.get_user_info),
     path('api-auth/', include('rest_framework.urls')),
     path('user/<int:chat_id>/', views.get_user, name='get_user'),
     path('user/<int:chat_id>/update/', views.update_user, name='update_user'),
